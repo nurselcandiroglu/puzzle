755159048e830eded41e0f95b062374e9d4bae35
nursel <nnurselcandiroglu@gmail.com>
2019-07-18 17:51:28 +0300

    initial

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button33 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button14);
+            this.panel1.Controls.Add(this.button15);
+            this.panel1.Controls.Add(this.button16);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 654);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 16;
+            this.button1.Tag = "2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 150);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(322, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 150);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(471, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 150);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(22, 178);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 150);
+            this.button5.TabIndex = 7;
+            this.button5.Tag = "8";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(172, 178);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 150);
+            this.button6.TabIndex = 6;
+            this.button6.Tag = "7";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(322, 178);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 150);
+            this.button7.TabIndex = 5;
+            this.button7.Tag = "6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(471, 178);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 150);
+            this.button8.TabIndex = 4;
+            this.button8.Tag = "5";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(22, 328);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 150);
+            this.button9.TabIndex = 11;
+            this.button9.Tag = "12";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(172, 328);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(150, 150);
+            this.button10.TabIndex = 10;
+            this.button10.Tag = "11";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(322, 328);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(150, 150);
+            this.button11.TabIndex = 9;
+            this.button11.Tag = "10";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(471, 328);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(150, 150);
+            this.button12.TabIndex = 8;
+            this.button12.Tag = "9";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(22, 479);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(150, 150);
+            this.button13.TabIndex = 15;
+            this.button13.Tag = "16";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(172, 479);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(150, 150);
+            this.button14.TabIndex = 14;
+            this.button14.Tag = "15";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(322, 479);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(150, 150);
+            this.button15.TabIndex = 13;
+            this.button15.Tag = "14";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(471, 479);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(150, 150);
+            this.button16.TabIndex = 12;
+            this.button16.Tag = "13";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.click);
+            // 
+            // button33
+            // 
+            this.button33.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button33.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button33.Location = new System.Drawing.Point(740, 252);
+            this.button33.Name = "button33";
+            this.button33.Size = new System.Drawing.Size(100, 47);
+            this.button33.TabIndex = 2;
+            this.button33.Text = "karıştır";
+            this.button33.UseVisualStyleBackColor = false;
+            this.button33.Click += new System.EventHandler(this.button33_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(719, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "en yüksek skor";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(740, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button17.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button17.Location = new System.Drawing.Point(740, 190);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(100, 47);
+            this.button17.TabIndex = 5;
+            this.button17.Text = "resim seç";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button19.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button19.Location = new System.Drawing.Point(740, 318);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(100, 47);
+            this.button19.TabIndex = 6;
+            this.button19.Text = "bitir";
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(954, 705);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button33);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "PUZZLE";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button33;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button19;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,298 @@
+﻿//160202004-nursel çandıroğlu
+//150202075-ayşin rodop
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        int x = 0;
+        Button[] bt = new Button[2];
+        Image temp;
+        Bitmap orginal;
+        List<Image> karismamis = new List<Image>();
+        List<Image> karismis = new List<Image>();
+        List<Image> karismis2 = new List<Image>();//karismis resimleri bu dizide tuttuk
+        int skor = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new System.EventHandler(click);
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private static void dosyayaYaz(int p)
+        {
+            string fileName = @"C:\Users\Nursel\Desktop\enyuksekskor.txt";
+            //dosya yolu bulundu
+            string writeText = Convert.ToString(p);
+
+            FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+
+            fs.Close();
+            File.AppendAllText(fileName, Environment.NewLine + writeText);
+            //son satıra ekler
+
+        }
+        private void dosyadanOku()
+        {
+
+            string dosya_yolu = @"C:\Users\Nursel\Desktop\enyuksekskor.txt";
+            //Okuma işlem yapacağımız dosyanın yolunu belirtiyoruz.
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            StreamReader sw = new StreamReader(fs);
+            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
+            string yazi = sw.ReadLine();
+
+            int skor, q = 0;
+            if (yazi != null && yazi != "")
+            {
+                skor = Convert.ToInt32(yazi);
+                q = skor;
+
+            }
+
+            string y;
+            while (yazi != null)
+            {
+                yazi = sw.ReadLine();
+                if (yazi != null && yazi != "")
+                {
+                    skor = Convert.ToInt32(yazi);
+
+                    if (q < skor)
+                    {
+                        q = skor;
+                    }
+                }
+
+            }
+            y = Convert.ToString(q);
+            textBox1.Text = y;
+
+            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
+            //Son satır okunduktan sonra okuma işlemini bitirdik
+            sw.Close();
+            fs.Close();
+            //İşimiz bitince kullandığımız nesneleri iade ettik.
+
+        }
+
+
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            dosyadanOku();
+
+            foreach (Button b in panel1.Controls)
+                b.Enabled = true;
+
+
+
+            //fotoğraf boyutunu 600*600 yaptık
+
+            if (karismis.Count == 0)
+                karismis = karismamis;
+
+            karismis2.Clear();// tekrar karıştırma işlemi yapabilmek için
+            butonlardaKarGoster();
+
+
+            yanlisSayisi = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                esitlik(karismis2[i], karismamis[i]);
+            }
+
+            Console.WriteLine("yanlis sasyısı::::::::::" + yanlisSayisi);
+            if (yanlisSayisi == 0)//tüm parçalar doğru yerdeyse tam puan verin
+            {
+
+                MessageBox.Show("tüm parçalar doğru yerde   SKOR:" + Convert.ToString(skor));
+
+                dosyayaYaz(skor);
+            }
+            if (yanlisSayisi == 16)//tüm parçalar yanlış yerdeyse bi daha karıştırma işlemi yapıyor.
+            {
+                MessageBox.Show("tekrar karıştır");
+                button19.Enabled = false;
+            }
+            else
+                button19.Enabled = true;
+
+        }
+
+        private void butonlardaKarGoster()
+        {
+            int i = 0;
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            arr = karistir(arr);
+
+            foreach (Button b in panel1.Controls)
+            {
+                if (i < arr.Length)
+                {
+                    b.Image = karismis[arr[i]];  // şurada butonlarda image atandığını gösteriyoruz
+                    karismis2.Add(karismis[arr[i]]); // karisik resimleri karisik2 dizisinde tutuyoruz.
+                    i++;
+                }
+
+
+            }
+
+        }
+
+        private int[] karistir(int[] arr)
+        {
+            Random r = new Random();
+            arr = arr.OrderBy(x => r.Next()).ToArray();
+            return arr;
+        }
+
+        private void parcala(Image orginal, int w, int h)
+        {
+            karismamis.Clear();//yeni resim açmak için
+            Bitmap bmp = new Bitmap(w, h);
+            Graphics graphic = Graphics.FromImage(bmp);
+
+            graphic.DrawImage(orginal, 0, 0, w, h);
+
+            int movr = 0, movd = 0;
+            for (int x = 0; x <= 15; x++)
+            {
+                Bitmap parca = new Bitmap(150, 150);
+
+                for (int i = 0; i < 150; i++)
+                    for (int j = 0; j < 150; j++)
+                        parca.SetPixel(i, j, bmp.GetPixel(i + movr, j + movd));
+                karismamis.Add(parca);
+                movr += 150;
+
+                if (movr == 600)
+                {
+                    movr = 0;
+                    movd += 150;
+
+                }
+
+            }
+
+        }
+
+
+        int yanlisSayisi = 0;
+        public void esitlik(Image img1, Image img2)
+        {
+            Bitmap rsm1 = new Bitmap(img1);
+            Bitmap rsm2 = new Bitmap(img2);
+            bool control = true;
+            for (int x = 0; x < 150; x++)
+            {
+                for (int y = 0; y < 150; y++)
+                {
+                    if (rsm1.GetPixel(x, y).ToString() != rsm2.GetPixel(x, y).ToString())
+                    {
+                        control = false;//hatalı pixel
+                    }
+                }
+            }
+
+            if (control == false)
+            {
+                yanlisSayisi++;
+            }
+
+        }
+
+        private void click(object sender, EventArgs e)
+        {
+            List<Image> asd = new List<Image>();
+            Button s = ((Button)sender);
+            s.Enabled = false;
+            x = 0;
+
+            foreach (Button b in panel1.Controls)
+            {
+                if (b.Enabled == false)
+                {
+                    bt[x] = b;
+
+
+                    if (x == 1)
+                    {
+                        temp = bt[0].Image;
+                        bt[0].Image = bt[1].Image;
+                        bt[1].Image = temp;
+
+                        foreach (Button ha in panel1.Controls)
+                        {
+                            asd.Add(ha.Image);
+                        }
+
+                        bt[0].Enabled = true;
+                        bt[1].Enabled = true;
+                    }
+                    x++;
+                }
+            }
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosya = new OpenFileDialog();
+            dosya.InitialDirectory = "C:\\Users\\Nursel\\Desktop";
+            dosya.ShowDialog();
+            String resim = dosya.FileName;
+            orginal = (Bitmap)Bitmap.FromFile(resim);
+            parcala(orginal, 600, 600);
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            //// butonlar yer değiştikçe listeyi güncelliyor
+            int i = 0;
+            foreach (Button b in panel1.Controls)
+            {
+                if (i < 16)
+                {
+                    karismis2[i] = b.Image;
+                    i++;
+                }
+            }
+            /////
+            yanlisSayisi = 0;
+            for (i = 0; i < 16; i++)
+            {
+                esitlik(karismis2[i], karismamis[i]);
+            }
+            skor = (16 - yanlisSayisi) * 6;
+
+            dosyayaYaz(skor);
+            MessageBox.Show(yanlisSayisi + " tane parça yanlış yerde    SKOR:" + Convert.ToString(skor));
+        }
+    }
+}
